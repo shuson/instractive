44ad458a0be62a05baec39479505f6ea0e5939ba
nevermoi <wlgq@hotmail.com>
2018-05-13 00:06:12 +0800

    use dummy api for login

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://api.nevermoi.com/instractive/v1/";
+export const API_URL =  "http://private-c70ca1-instractiveapp.apiary-mock.com" //"http://api.nevermoi.com/instractive/v1/";
 export const FB_APP_ID = "203097063633202";
